831fed86a902a55188435a5e4f13579ed2e1591a
tirthosarkar <tirthosarkar1@gmail.com>
2026-03-27 21:01:48 +0600

    added if.js

---
+++ if.js
@@ -0,0 +1,55 @@
+/**
+ * Tui jodi amake khawas
+ * taile ami asbo
+ * 
+ * ..................
+ * if you food for me
+ *      i will come
+ * 
+ * ...................
+ * if briyani price less than 300 taka
+ *    i will buy
+ * 
+ * 
+ * -----STRUCTURE---------
+ * if (condition){
+ * 
+ * }
+ * 
+ * -----------------------
+ * if (condition){
+ * 
+ * }
+ * 
+ * 
+ * 
+ */
+
+
+
+if (3 < 20) {
+      console.log('i have a samaller value');
+}
+
+var briyaniPrice = 250;
+
+
+if (briyaniPrice < 300) {
+  console.log('mama give me some biryani')
+}
+
+
+/**
+ * if (condition){
+ * execute code if the condition is false
+ * }
+ */
+
+var weight = 40
+if (weight < 20) {
+  console.log('I will carry it by myself');
+}
+
+else {
+  console.log('I will rent a rickshaw')
+}
