768eacf804275859fa05bd381c9847ac895acbca
tirthosarkar <tirthosarkar1@gmail.com>
2026-03-31 14:07:53 +0600

    added all code

--- shortcut.js
+++ shortcut.js
@@ -1,7 +1,18 @@
 const isLeader = true;
-if (isLeader === true) {
-  console.log('leader asche! chair char')
+// if (isLeader === true)
+// if(!isLeader){
+//   console.log('leader asche! chair char')
+// }
+// else {
+//   console.log('vitore dukte dibi na');
+// }
+
+
+const isPassed = false;
+// if (isPassed === false)
+if(!!isPassed){
+  console.log('toke biye diya dimu');
 }
 else {
-  console.log('vitore dukte dibi na');
+  console.log('tumi amder noyon er moni')
 }
